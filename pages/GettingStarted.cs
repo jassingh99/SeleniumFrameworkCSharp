5c9de5695e2f0499b9f4f68504d2fa2c0f5514e1
Jasbir Singh <jassingh99@gmail.com>
2023-11-29 12:08:27 -0500

    Checking in framework skeleton (project, config and driver initialization code)

---
+++ pages/GettingStarted.cs
@@ -0,0 +1,19 @@
+﻿using OpenQA.Selenium;
+
+namespace SeleniumFramework.pages
+{
+    class GettingStarted
+    {
+        private IWebDriver driver = null;
+
+        private By pageImageClass = By.XPath("//h1[contains(text(),'List of features')]");
+        private By backToTop = By.ClassName("back-to-top");
+        private By legalLink = By.LinkText("Legal");
+        public GettingStarted(IWebDriver d)
+        {
+            driver = d;
+
+        }
+
+    }
+}
---
+++ util/DriverManager.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Remote;
+
+namespace SeleniumFramework.util
+{
+    internal class DriverManager
+    {
+        //code to manager driver instances
+        public WebDriver driver { get; set; }
+        public DriverManager()
+        {
+
+        }
+        public WebDriver getDriver(BrowserConfig browserConfig)
+        {
+            string gridUrl = Environment.GetEnvironmentVariable("gridUrl");
+            string browserName = Environment.GetEnvironmentVariable("browserName");
+            if(gridUrl==null){
+                TestContext.WriteLine("gridUrl command line parameter is not provided. Launching driver using local browser.");
+            }
+            if(browserName==null){
+                TestContext.WriteLine("Browser Name is empty. Please provide one of the values for browserName [chrome, edge, firefox]");
+                Assert.Fail();
+            }
+            switch (browserName)
+            {
+                case ("chrome"):
+                    ChromeOptions chromeOptions = new ChromeOptions();
+                    
+                    if (browserConfig.Headless)
+                    {
+                        chromeOptions.AddArgument("--headless");
+                    }
+                    chromeOptions.AddArgument("--window-size=1920,1080");
+                    chromeOptions.AddArgument("--ignore-certificate-errors");
+                    if(gridUrl==null){
+                        driver = new ChromeDriver(chromeOptions);
+                    }else{
+                        driver = new RemoteWebDriver(new Uri(gridUrl), chromeOptions);
+                    }
+                    break;
+
+                default:
+                    break;
+            }
+            return driver;
+        }
+    }
+}
