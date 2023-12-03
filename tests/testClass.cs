3656b02613dfe9a9187b9d42492407f602d0c6df
Jasbir Singh <jassingh99@gmail.com>
2023-12-03 14:02:09 -0500

    Driver intialization logic is added. Basic Test cases added Updated gitignore fle Config file to manage the test url, browsertype, logging info etc. added

---
+++ tests/testClass.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using SeleniumFramework.util;
+using System.Runtime;
+using SeleniumFramework.pages;
+using FluentAssertions;
+namespace SeleniumFramework.tests
+{
+    [TestFixture]
+
+    class testClass
+    {
+        IWebDriver driver = null;
+        GettingStarted gettingStarted = null;
+        Home home = null;
+        TestDataManager testData = null;
+        DriverManager dm = new DriverManager();
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Main");
+        }
+        [OneTimeSetUp]
+        public void initialize()
+        {
+            testData = TestDataManager.GetInstance;
+            driver = dm.getDriver(testData.browserConfig);
+            gettingStarted = new GettingStarted(driver);
+            home = new Home(driver);
+        }
+        [Test]
+        public void testHomePage()
+        {
+            TestContext.WriteLine(String.Format("Launching App {0}", testData.AppURL));
+            home.openHome(testData.AppURL);
+            home.isHomePageLoaded().Should().BeTrue();
+            TestContext.WriteLine("App is launched successfully");
+        }
+        [Test]
+        public void testGoToFeaturePage()
+        {
+            home.openHome(testData.AppURL);
+            TestContext.WriteLine("App is launched successfully");
+            home.clickResourceMenu().Should().BeTrue();
+            TestContext.WriteLine("Clicked on Resource Menu");
+            home.clickFeatureMenu().Should().BeTrue();
+            TestContext.WriteLine("Clicked on Feature Menu");
+        }
+        [Test]
+        public void testFeaturePage()
+        {
+            home.openHome(testData.AppURL);
+            TestContext.WriteLine("App is launched successfully");
+            home.clickResourceMenu().Should().BeTrue();
+            TestContext.WriteLine("Clicked on Resource Menu");
+            home.clickFeatureMenu().Should().BeTrue();
+            TestContext.WriteLine("Clicked on Feature Menu");
+            gettingStarted.isFeaturePageLoaded().Should().BeTrue();
+            gettingStarted.clickLegalLink().Should().BeTrue();
+            TestContext.WriteLine("Clicked on Legal Link");
+            gettingStarted.clickBackToTop().Should().BeTrue();
+            TestContext.WriteLine("Feature Page testing completed");
+        }
+        [OneTimeTearDown]
+        public void cleanUp()
+        {
+            driver.Quit();
+        }
+    }
+}
